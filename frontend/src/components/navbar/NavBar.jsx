02fe354714a743268b75e8516904263e7818e1a6
Meeri <meeri.mariella@gmail.com>
2024-10-08 13:26:10 +0300

    New-button redirects to exercise list and myprograms and icon redirect to program list page.

--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -3,16 +3,24 @@ import { AppBar, Toolbar, Typography, Button, IconButton, Box, Tooltip } from '@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter'
 
+const handleCreateNewClick = () => {
+  window.location.href = '/exercises'
+}
+
+const handleBackToFrontpage = () => {
+  window.location.href = '/'
+}
+
 const NavBar = () => {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#efa37f' }}>
       <Toolbar sx={{ justifyContent: 'space-between' }}>
         <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <FitnessCenterIcon sx={{ fontSize: 40, marginRight: 3 }} />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, marginRight: 3 }}>
+          <FitnessCenterIcon onClick={() => handleBackToFrontpage()} sx={{ fontSize: 40, marginRight: 3 }} />
+          <Typography onClick={() => handleBackToFrontpage()} variant="h6" component="div" sx={{ flexGrow: 1, marginRight: 3 }}>
             My programs
           </Typography>
-          <Button variant="contained" sx={{ color: '#efa37f', backgroundColor: 'white', ':hover': { backgroundColor: 'lightgray' } }}>New</Button>
+          <Button onClick={() => handleCreateNewClick()} variant="contained" sx={{ color: '#efa37f', backgroundColor: 'white', ':hover': { backgroundColor: 'lightgray' } }}>New</Button>
         </Box>
         <Tooltip title="Logged in">
           <IconButton color="inherit">
